c063da8b6efdd5c4eda148666e450dd1a65682f8
Mr. Bickelsworth <abickel@larsontexts.com>
2025-09-19 15:45:18 -0400

    udpates console output for step-1-outcomes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("");
-Console.WriteLine("************** Step 1 **************");
-Console.WriteLine("Open './Docs/requirements-analysis.readme.md' for more information.");
-Console.WriteLine("To skip ahead to the solution for this step, run ''git checkout step-1-requirements-analysis-outcome && dotnet run'");
+Console.WriteLine("************** Step 1 Outcome **************");
+Console.WriteLine("Open './Outcomes/step-1-requirements-analysis.md' for more information.");
 Console.WriteLine("To continue to the next step, run 'git checkout step-2-api-design && dotnet run'");
     
