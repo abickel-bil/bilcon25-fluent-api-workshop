94628955fce886ba1f6db14a67b3bb08edb073d6
Mr. Bickelsworth <abickel@larsontexts.com>
2025-09-19 15:33:48 -0400

    adds workshop objectives to overview step

--- Program.cs
+++ Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine("");
 Console.WriteLine("************** Overview **************");
 Console.WriteLine("Welcome to the overview");
 Console.WriteLine("Open './Docs/overview.readme.md' for more information.");
-Console.WriteLine("When you're ready to continue, run 'git checkout step-1-requirements-analysis && dotnet run' to continue");
+Console.WriteLine("When you're ready to continue, run 'git checkout step-1-requirements-analysis' followed by 'dotnet run' to continue");
