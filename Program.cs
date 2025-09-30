2cf7c08b0b792b07dd1f676746c6c240c8793ca9
Mr. Bickelsworth <abickel@larsontexts.com>
2025-09-30 09:32:44 -0400

    adds console output for step two outcome

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("");
-Console.WriteLine("************** Step 2 **************");
-Console.WriteLine("Open 'Docs/step-2-api-design.md' for details.");
-Console.WriteLine("To skip to the outcome for this step, run 'git checkout step-2-api-design-outcome && dotnet run'");
+Console.WriteLine("************** Step 2 Outcome **************");
+Console.WriteLine("Open './Outcomes/step-2-api-design.md' for more information.");
 Console.WriteLine("To continue to the next step, run 'git checkout step-3-implementation && dotnet run'");
