99e05d945bef6d87c56327b5b4bdcfdb9ce55830
Mr. Bickelsworth <abickel@larsontexts.com>
2025-09-30 10:50:21 -0400

    initial guidance for step 3 outcome

---
+++ Outcomes/step-3-implementation.md
@@ -0,0 +1,4 @@
+# Step 3: Implementation Outcome
+
+- review `/src/interfaces` to see interface definitions for the API.
+- review `/src/builder` to see a complete implementation.
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("");
-Console.WriteLine("************** Step 3 - Implementation **************");
-Console.WriteLine("Open './Docs/step-3-implementation.md' for more information.");
-Console.WriteLine("To skip to the outcome for this step, `git checkout step-3-implementation-outcome && dotnet run`");
+Console.WriteLine("************** Step 3 Outcome **************");
+Console.WriteLine("Open './Outcomes/step-3-implementation.md' for more information.");
 Console.WriteLine("To continue to the summary, `git checkout summary && dotnet run`");
